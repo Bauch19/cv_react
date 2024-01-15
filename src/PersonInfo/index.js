f3efda0d4859fb0dded64200e2feb083d9b9f784
Bauch19 <112658412+Bauch19@users.noreply.github.com>
2024-01-15 00:09:31 -0600

    Responsive
    Sección de programas
    Enlaces

--- src/PersonInfo/index.js
+++ src/PersonInfo/index.js
@@ -72,19 +72,116 @@ const PersonInfo = () =>{
                         <Col lg="6" sm="12">
                             <div className='proyectos-galeria'>
                                 <Row>
-                                    <Col lg='6'><div className='proyecto-square'></div></Col>
-                                    <Col lg='6'><div className='proyecto-square'></div></Col>
+                                    <Col lg='6' sm='6'><div className='proyecto-square'></div></Col>
+                                    <Col lg='6' sm='6'><div className='proyecto-square'></div></Col>
                                 </Row>
                                 <Row>
-                                    <Col lg='6'><div className='proyecto-square'></div></Col>
-                                    <Col lg='6'><div className='proyecto-square'></div></Col>
+                                    <Col lg='6' sm='6'><div className='proyecto-square'></div></Col>
+                                    <Col lg='6' sm='6'><div className='proyecto-square'></div></Col>
                                 </Row>
                             </div>
+                        </Col>
+                    </Row>
+                    {/*Esta seccion es de proyectos destacados*/}
+                    <Row className='proyecto-sec-galeria'>
+                        <Col lg="6" sm="12">
+                            <div className='proyectos-galeria destacados'>
+                                <Row>
+                                    <Col lg='6' sm='6'><div className='proyecto-square'></div></Col>
+                                    <Col lg='6' sm='6'><div className='proyecto-square'></div></Col>
+                                </Row>
+                            </div>
+                        </Col>
+                        <Col lg="6" sm="12">
+                            <div className='proyectos'>
+                                <div className="proyecto-titulo">Destacados</div>
+                                <p className='seccionProyecto'>Proyectos Destacados</p>
+                                <p className='info-proyecto'>Los proyectos destacados del último año que desarrolle</p>
+                            </div>
+                        </Col>
+                    </Row>
+                    {/*Sección de programas*/}
+                    <Row className='sec-programas'>
+                        <Col lg='2' md='4' sm='6'>
+                            <img src={require('../img/programas/github-gray.png')} alt='program' className='img-program' />
+                        </Col>
+                        <Col lg='2' md='4' sm='6'>
+                            <img src={require('../img/programas/illustrator-gray.png')} alt='program' className='img-program' />
+                        </Col>
+                        <Col lg='2' md='4' sm='6'>
+                            <img src={require('../img/programas/vs-gray.png')} alt='program' className='img-program' />
+                        </Col>
+                        <Col lg='2' md='4' sm='6'>
+                            <img src={require('../img/programas/photoshop-gray.png')} alt='program' className='img-program' />
+                        </Col>
+                        <Col lg='2' md='4' sm='6'>
+                            <img src={require('../img/programas/mamp-gray.png')} alt='program' className='img-program' />
+                        </Col>
+                        <Col lg='2' md='4' sm='6'>
+                            <img src={require('../img/programas/premiere-gray.png')} alt='program' className='img-program' />
+                        </Col>
+                    </Row>
+                    {/*Sección de contacto*/}
+                    <Row className='contacto-sec'>
+                        <Col lg='12' sm='12'>
+                            <div className='contacto-info'>
+                                <p>
+                                ¿Te gusta algo de lo que ves?, ¿Tienes alguna pregunta?
+                                </p>
+                            </div>
+                        </Col>
+                    </Row>
+                    <Row className='contacto-footer'>
+                        <Col lg='6' sm='6'>
+                            <div className='contacto-msj'>
+                                <p>Contacta me y podré resolver tus dudas</p>
+                            </div>
+                        </Col>
+                        <Col lg='6' sm='6'>
+                            <a href='https://www.linkedin.com/in/enrique-bouchet-b873b9285'>
+                                <button className='contacto-btn'>
+                                    Contacto <img src={require('../img/flecha.png')} alt='CV' className='flecha-contacto' />
+                                </button>
+                            </a>
                         </Col>
                     </Row>
                 </div>
             </div>
             </Container>
+            <footer>
+                <Container>
+                    <Row>
+                        <Col lg="6" sm='6'>
+                            <h2>Bauch Portafolio</h2>
+                        </Col>
+                        <Col lg="6" sm='6'>
+                            <Row>
+                                <Col lg='4' sm='4'>
+                                    <h5>Redes Sociales</h5>
+                                    <Row className='redes'>
+                                        <a className='red' href='https://www.facebook.com/bauch.ventura'>Facebook</a>
+                                        <a className='red' href='https://www.instagram.com/bauchventura/'>Instagram</a>
+                                        <a className='red' href='https://www.linkedin.com/in/enrique-bouchet-b873b9285'>LinkedIn</a>
+                                    </Row>
+                                </Col>
+                                <Col lg='4' sm='4'>
+                                    <h5>Enlaces</h5>
+                                    <Row className='redes'>
+                                    <a className='red' href='https://github.com/Bauch19/cv_react'>Git Hub</a>
+                                        <a className='red' href='https://drive.google.com/file/d/18H-wCZXIZHsdeJKWcQMsnL-nyJSc8Ruu/view?usp=sharing'>Drive</a>
+                                    </Row>
+                                </Col>
+                                <Col lg='4' sm='4'>
+                                    <h5>Curriculum</h5>
+                                    <Row className='redes'>
+                                        <a className='red' href='https://drive.google.com/file/d/18H-wCZXIZHsdeJKWcQMsnL-nyJSc8Ruu/view?usp=sharing'>Curriculum Vitae</a>
+                                    </Row>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Container>
+            </footer>
         </body>
     );
 }
